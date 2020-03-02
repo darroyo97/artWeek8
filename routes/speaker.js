a10b013d8df8b10d416418fdb9103a6a3c9d0fd0
Daniela <daniela.arroyo27@gmail.com>
2020-03-02 15:02:59 -0600

    adding data from json to page

--- routes/speaker.js
+++ routes/speaker.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
+let data = require('../data/data.json')
+
 
 router.get('/speaker', (req, res) => {
-    res.render('speaker')
+    let pagePhotos = []
+    data.speakers.forEach((speakerObj) => {
+        pagePhotos = pagePhotos.concat(speakerObj.artwork)
+    })
+    res.render('speaker', {
+        pageTitle: 'Speaker List',
+        artistName: "",
+        artwork: pagePhotos,
+        allSpeakers: data.speakers,
+        pageID: 'speakerList'
+    })
+})
+router.get('/speaker/:speakerID', (req, res) => {
+    res.render('speakers')
 })
 
 module.exports = router;
