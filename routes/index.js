4cf3a476f6de6bf8ca61ece2e860c731597776bb
Daniela <daniela.arroyo27@gmail.com>
2020-03-02 14:27:02 -0600

    looping through imgs and displaying them

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+let data = require('../data/data.json')
 router.get('/', (req, res) => {
-    res.render('index')
+
+    let pagePhotos = [];
+    data.speakers.forEach((speaker) => {
+        pagePhotos = pagePhotos.concat(speaker.artwork)
+    })
+    console.log(pagePhotos)
+
+
+    res.render('index', {
+        artwork: pagePhotos
+    })
 })
 
 
