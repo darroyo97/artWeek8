cb718b76fac7e414fbfd30bf7cb9902593044122
Daniela <daniela.arroyo27@gmail.com>
2020-03-04 14:45:36 -0600

    feedback not working

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const body_parser = require('body-parser');
 let feedbackData = require('../data/feedback.json')
 router.use(body_parser.urlencoded({ extended: false }));
+const fs = require('fs')
 
 router.get('/api', (req, res) => {
     res.send('json data')
@@ -13,10 +14,11 @@ router.post('/api', (req, res) => {
     //take data from our file
 
     feedbackData.unshift(req.body)
-    fs.writeFile('../data/feedback.json', JSON.stringify(feedbackData), 'utf8', (err) => {
+    fs.writeFile('data/feedback.json', JSON.stringify(feedbackData), 'utf8', (err) => {
         if (err) {
             console.log(err)
         }
+        console.log(req.body)
         res.json(feedbackData)
     })
     res.json({
